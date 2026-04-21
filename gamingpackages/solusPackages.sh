d7641f6ecb33331ec1d86623580db67ca99fc9f5
Adam Taylor <adamtaylor88@hotmail.co.uk>
2026-04-21 15:19:24 +0100

    added .sh files for solus & opensuse

---
+++ gamingpackages/solusPackages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Set system to use bash for script
+
+# Sets script to stop at any errors
+set -e
+
+# list of packages to be installed in an array form
+packages=(
+  steam
+  heroic-games-launcher
+  mangohud
+  mangohud-32bit
+  goverlay
+  mesa
+  mesa-32bit
+  vulkan
+  vulkan-32bit
+  steam-devices
+  inputplumber
+  sdl2
+  sdl2-32bit
+  pipewire
+  pipewire-alsa
+  pipewire-pulse
+  wireplumber
+  alsa-utils
+  discord
+)
+
+# Prints the list of packages that will be installed
+echo "Packages to install:"
+printf '  - %s\n' "${packages[@]}"
+
+# Does a dry run of the packages to install to check for any errors
+echo "Performing dry run (no changes will be made)..."
+sudo eopkg install --dry-run "${packages[@]}"
+echo
+
+# Prompts user to confirm or deny for installation to go ahead
+read -p "Proceed with installation? [y/N] " answer
+[[ "$answer" == "y" ]] || exit 0
+
+# Performs install of packages
+sudo eopkg install "${packages[@]}"
+
+# Prints after installation has finished
+echo "Installation complete"
